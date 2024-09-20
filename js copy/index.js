fd03792a9e383b8cf520f8608c5bb61b23d82499
Mickael0997 <158517216+Mickael0997@users.noreply.github.com>
2024-09-20 21:39:52 +0000

    delete copy

--- js copy/index.js
+++
@@ -1,8 +0,0 @@
-console.log("Bienvenue dans mon application JavaScript optimisée!");
-
-// Exemple de fonction
-function greet(name) {
-  return `Bonjour, ${name}!`;
-}
-
-console.log(greet("Utilisateur"));
